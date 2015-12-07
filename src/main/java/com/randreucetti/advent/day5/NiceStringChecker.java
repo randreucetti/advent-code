35de6d7aa232fb9d3092167d4ad93f91b11dbc1b
randreucetti <ross.andreucetti@gmail.com>
2015-12-07 09:58:26 +0800

    day 5

--- src/main/java/com/randreucetti/advent/day5/NiceStringChecker.java
+++ src/main/java/com/randreucetti/advent/day5/NiceStringChecker.java
@@ -1,16 +1,17 @@
 package com.randreucetti.advent.day5;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class NiceStringChecker {
-	public static final char[] VOWELS = { 'a', 'e', 'i', 'o', 'u' };
-	public static final List<Character> VOWELS_LIST = new ArrayList<Character>();
+    private static final List<Character> VOWELS_LIST = Arrays.asList('a', 'e', 'i', 'o', 'u');
+    private static final List<String> FORBIDDEN_LIST = Arrays.asList("ab", "cd", "pq", "xy");
 
-	public static final String[] FORBIDDEN = { "ab", "cd", "pq", "xy" };
-	public static final List<String> FORBIDDEN_LIST = new ArrayList<String>();
+    private static final Pattern P2_PATTERN1 = Pattern.compile("(..).*\\1");
+    private static final Pattern P2_PATTERN2 = Pattern.compile("(.).\\1");
 
-	public static boolean isNice(String input) {
+    public static boolean isNiceP1(String input) {
 		int numVowels = 0;
 		char prev = 0;
 		boolean twoInARow = false;
@@ -18,23 +19,24 @@ public class NiceStringChecker {
 		for (int i = 0; i < input.length(); i++) {
 			if (VOWELS_LIST.contains(input.charAt(i))) {
 				numVowels++;
-				if (numVowels == 3) {
-					return true;
-				}
 			}
 			if (prev == input.charAt(i)) {
 				twoInARow = true;
 			}
 
 			if (i < input.length() - 1) {
-				if (FORBIDDEN_LIST.contains(input.substring(i, i + 1))) {
+                if (FORBIDDEN_LIST.contains(input.substring(i, i + 2))) {
 					containsForbidden = true;
 				}
 			}
 
 			prev = input.charAt(i);
 		}
-		
-		return numVowels >=3 &&twoInARow && !containsForbidden;
+
+        return numVowels >= 3 && twoInARow && !containsForbidden;
 	}
+
+    public static boolean isNiceP2(String input) {
+        return P2_PATTERN1.matcher(input).find() && P2_PATTERN2.matcher(input).find();
+    }
 }
--- src/test/java/com/randreucetti/advent/day5/NiceStringCheckerTest.java
+++ src/test/java/com/randreucetti/advent/day5/NiceStringCheckerTest.java
@@ -9,24 +9,46 @@ import org.junit.Test;
 
 public class NiceStringCheckerTest {
 	@Test
-	public void testGetNumNiceStrings() {
-		File file = new File(this.getClass().getClassLoader()
-				.getResource("day5/input.txt").getFile());
-		int counter = 0;
-		try (Scanner scanner = new Scanner(file)) {
-			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				if(NiceStringChecker.isNice(line)){
-					counter++;
-				}
-			}
+    public void testGetNumNiceP1Strings() {
+        File file = new File(this.getClass().getClassLoader()
+                .getResource("day5/input.txt").getFile());
+        int counter = 0;
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (NiceStringChecker.isNiceP1(line)) {
+                    counter++;
+                }
+            }
 
-			scanner.close();
-			System.out.println("Number nice strings: "
-					+ counter);
+            scanner.close();
+            System.out.println("Number nice strings (Part 1): "
+                    + counter);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
+
+    @Test
+    public void testGetNumNiceP2Strings() {
+        File file = new File(this.getClass().getClassLoader()
+                .getResource("day5/input.txt").getFile());
+        int counter = 0;
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (NiceStringChecker.isNiceP2(line)) {
+                    counter++;
+                }
+            }
+
+            scanner.close();
+            System.out.println("Number nice strings (Part 2): "
+                    + counter);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
